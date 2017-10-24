a1018e87f06796d11a04762a2715a5803384873a
adelavegaf <antoniodlavga@gmail.com>
2017-10-24 17:50:46 -0500

    Add get goal status method

--- imports/api/goals.js
+++ imports/api/goals.js
@@ -4,9 +4,25 @@ import {check} from 'meteor/check';
 
 const Goals = new Mongo.Collection('goals');
 
+function augmentGoalWithStatus(goal, sinceDate) {
+    const [usage] = Meteor.call('visits.getDomainUsage', goal.hostname, sinceDate);
+    goal.timeSpent = usage ? usage.total : 0;
+    switch (goal.quantifier) {
+        case '<':
+            goal.isFailing = goal.timeSpent > goal.timeGoal;
+            break;
+        case '>':
+            goal.isFailing = goal.timeSpent < goal.timeGoal;
+            break;
+    }
+}
+
 if (Meteor.isServer) {
     Meteor.methods({
         getGoalsStatus(sinceDate) {
+            if (!Meteor.userId()) {
+                throw new Meteor.Error('not-authorized');
+            }
             // Extension uses Asteroid that serializes date differently than Meteor. Must perform this check.
             if (!(sinceDate instanceof Date)) {
                 sinceDate = new Date(sinceDate);
@@ -15,25 +31,47 @@ if (Meteor.isServer) {
             const userGoals = Meteor.call('getGoals');
 
             for (const goal of userGoals) {
-                const [usage] = Meteor.call('visits.getDomainUsage', goal.hostname, sinceDate);
-                goal.timeSpent = usage ? usage.total : 0;
-                switch (goal.quantifier) {
-                    case '<':
-                        goal.isFailing = goal.timeSpent > goal.timeGoal;
-                        break;
-                    case '>':
-                        goal.isFailing = goal.timeSpent < goal.timeGoal;
-                        break;
-                }
+                augmentGoalWithStatus(goal, sinceDate);
             }
+
             return userGoals;
+        },
+        getGoalStatus(hostname, sinceDate) {
+
+            if (!Meteor.userId()) {
+                throw new Meteor.Error('not-authorized');
+            }
+
+            if (!(sinceDate instanceof Date)) {
+                sinceDate = new Date(sinceDate);
+            }
+
+            const [goal] = Meteor.call('getGoal', hostname);
+
+            if (!goal) {
+                throw new Meteor.Error('No goal associated with this hostname');
+            }
+
+            augmentGoalWithStatus(goal, sinceDate);
+
+            return goal;
         }
     });
 }
 
 Meteor.methods({
     getGoals() {
+        if (!Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
         return Goals.find({owner: this.userId}).fetch();
+    },
+    getGoal(hostname) {
+        check(hostname, String);
+        if (!Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+        return Goals.find({owner: this.userId, hostname: hostname}).fetch();
     },
     addGoal(hostname, quantifier, timeGoal) {
         check(hostname, String);
@@ -42,6 +80,12 @@ Meteor.methods({
 
         if (!Meteor.userId()) {
             throw new Meteor.Error('not-authorized');
+        }
+
+        const [goal] = Meteor.call('getGoal', hostname);
+
+        if (goal) {
+            throw new Meteor.Error('This hostname already has a goal');
         }
 
         const date = new Date();
@@ -58,11 +102,11 @@ Meteor.methods({
     removeGoal(id) {
         check(id, String);
 
-        const goal = Goals.findOne(id);
-
         if (!Meteor.userId()) {
             throw new Meteor.Error('not-authorized');
         }
+
+        const goal = Goals.findOne(id);
 
         if (goal.owner !== Meteor.userId()) {
             throw new Meteor.Error('not-authorized');
