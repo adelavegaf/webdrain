8d0cc2515c1dfdaf5caa5e69588fd62c6fcb9822
adelavegaf <antoniodlavga@gmail.com>
2017-10-28 11:31:46 -0500

    Add date filter UI tests.

--- imports/ui/components/DateFilter.test.js
+++ imports/ui/components/DateFilter.test.js
@@ -3,11 +3,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { chai } from 'meteor/practicalmeteor:chai';
 import DateFilter from './DateFilter.js';
+import MenuItem from 'material-ui/MenuItem';
 
 describe('DateFilter', () => {
-    it('should render', () => {
+    it('should render top level div', () => {
         const dateFilter = shallow(<DateFilter currentDateFilter={'day'} setCurrentDateFilter={() => console.log()}/>);
         chai.assert(dateFilter.hasClass('date-selector'));
+    });
 
+    it('should render 4 menu items', () => {
+        const dateFilter = shallow(<DateFilter currentDateFilter={'day'} setCurrentDateFilter={() => console.log()}/>);
+        chai.assert.equal(dateFilter.find(MenuItem).length, 4);
     });
 });
