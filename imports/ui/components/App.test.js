186ed2689be975d7a8ee008160ae957d46505d66
adelavegaf <antoniodlavga@gmail.com>
2017-10-28 12:06:11 -0500

    Add app unit tests.

--- imports/ui/components/App.test.js
+++ imports/ui/components/App.test.js
@@ -3,26 +3,35 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {chai} from 'meteor/practicalmeteor:chai';
 import App from './App.js';
-import {AppBar, FlatButton} from 'material-ui';
+import {AppBar} from 'material-ui';
+import StatisticsContainer from '../containers/StatisticsContainer';
+import LandingPageContainer from '../containers/LandingPageContainer';
 
 if (Meteor.isClient) {
     describe('App', function () {
         it('should render AppBar', function () {
-            const app = shallow(<App onLogOut={() => console.log('bom')} onLogIn={() => console.log('bom')}
-                                            onRegister={() => console.log('bom')} currentUser={'user'}/>);
+            const app = shallow(<App currentUser={'user'}/>);
             chai.assert.equal(app.find(AppBar).length, 1);
         });
 
-        it('should render log out button in AppBar when current user is defined', function () {
-            const app = shallow(<App onLogOut={() => console.log('bom')} onLogIn={() => console.log('bom')}
-                                     onRegister={() => console.log('bom')} currentUser={'user'}/>);
-            chai.assert.equal(app.find(FlatButton).length, 1);
+        it('should render statistics container when current user is defined', function () {
+            const app = shallow(<App currentUser={'user'}/>);
+            chai.assert.equal(app.find(StatisticsContainer).length, 1);
         });
 
-        it('should not render log out button in AppBar when current user is not defined', function () {
-            const app = shallow(<App onLogOut={() => console.log('bom')} onLogIn={() => console.log('bom')}
-                                     onRegister={() => console.log('bom')} currentUser={null}/>);
-            chai.assert.equal(app.find(FlatButton).length, 0);
+        it('should not render statistics container when current user is not defined', function () {
+            const app = shallow(<App currentUser={null}/>);
+            chai.assert.equal(app.find(StatisticsContainer).length, 0);
+        });
+
+        it('should render landing page container when current user is not defined', function () {
+            const app = shallow(<App currentUser={null}/>);
+            chai.assert.equal(app.find(LandingPageContainer).length, 1);
+        });
+
+        it('should not render landing page container when current user is defined', function () {
+            const app = shallow(<App currentUser={'user'}/>);
+            chai.assert.equal(app.find(LandingPageContainer).length, 0);
         });
     });
 }
