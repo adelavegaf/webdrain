d29361aa7e2ed456830541efc6f437ec2491607c
adelavegaf <antoniodlavga@gmail.com>
2017-10-31 12:32:23 -0500

    Improve landing page UI.

--- imports/ui/components/LandingPage.js
+++ imports/ui/components/LandingPage.js
@@ -9,6 +9,20 @@ const loginPromptStyle = {
     color: '#737976'
 };
 
+const titleStyle = {
+    paddingTop: '20px',
+    fontWeight: 300,
+    fontSize: '22px',
+    color: 'rgba(255, 255, 255, 0.94)'
+};
+
+const subtitleStyle = {
+    paddingBottom: '10px',
+    fontWeight: 300,
+    fontSize: '18px',
+    color: 'rgba(255, 255, 255, 0.94)'
+};
+
 const buttonContainer = {
     marginRight: '5px',
     marginLeft: '5px'
@@ -27,47 +41,57 @@ const buttonsContainer = {
     paddingTop: '20px'
 };
 
+const loginPromptContainerStyle = {
+    background: '#009faf'
+};
+
 export default class LandingPage extends Component {
     render() {
         return (
-            <div>
-                <Card>
-                    <CardText>
-                        <div style={loginPromptStyle}>
-                            Welcome
-                        </div>
-                        <div className="row justify-content-center" style={textFieldContainer}>
-                            <TextField type="text"
-                                       floatingLabelText="Username"
-                                       floatingLabelFixed={true}
-                                       errorText={this.props.authenticationError}
-                                       value={this.props.username}
-                                       onChange={(e) => this.props.onUserNameChange(e.target.value)}/>
-                        </div>
-                        <div className="row justify-content-center" style={textFieldContainer}>
-                            <TextField type="password"
-                                       floatingLabelText="Password"
-                                       floatingLabelFixed={true}
-                                       errorText={this.props.authenticationError}
-                                       value={this.props.password}
-                                       onChange={(e) => this.props.onPasswordChange(e.target.value)}/>
-                        </div>
-                        <div className="row justify-content-center" style={buttonsContainer}>
-                            <div style={buttonContainer}>
-                                <RaisedButton label="Log In"
-                                              secondary={true}
-                                              labelStyle={labelButtonStyle}
-                                              onClick={() => this.props.onLogIn()}/>
-                            </div>
-                            <div style={buttonContainer}>
-                                <RaisedButton label="Register"
-                                              secondary={true}
-                                              labelStyle={labelButtonStyle}
-                                              onClick={() => this.props.onRegister()}/>
-                            </div>
-                        </div>
-                    </CardText>
-                </Card>
+            <div className="row justify-content-center" style={loginPromptContainerStyle}>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <h2 style={titleStyle}>We help you manage your time in the web</h2>
+                    <h2 style={subtitleStyle}>Set website usage daily goals and start getting insights today!</h2>
+                    <div>
+                        <Card>
+                            <CardText>
+                                <div style={loginPromptStyle}>
+                                    Welcome
+                                </div>
+                                <div className="row justify-content-center" style={textFieldContainer}>
+                                    <TextField type="text"
+                                               floatingLabelText="Username"
+                                               floatingLabelFixed={true}
+                                               errorText={this.props.authenticationError}
+                                               value={this.props.username}
+                                               onChange={(e) => this.props.onUserNameChange(e.target.value)}/>
+                                </div>
+                                <div className="row justify-content-center" style={textFieldContainer}>
+                                    <TextField type="password"
+                                               floatingLabelText="Password"
+                                               floatingLabelFixed={true}
+                                               errorText={this.props.authenticationError}
+                                               value={this.props.password}
+                                               onChange={(e) => this.props.onPasswordChange(e.target.value)}/>
+                                </div>
+                                <div className="row justify-content-center" style={buttonsContainer}>
+                                    <div style={buttonContainer}>
+                                        <RaisedButton label="Log In"
+                                                      secondary={true}
+                                                      labelStyle={labelButtonStyle}
+                                                      onClick={() => this.props.onLogIn()}/>
+                                    </div>
+                                    <div style={buttonContainer}>
+                                        <RaisedButton label="Register"
+                                                      secondary={true}
+                                                      labelStyle={labelButtonStyle}
+                                                      onClick={() => this.props.onRegister()}/>
+                                    </div>
+                                </div>
+                            </CardText>
+                        </Card>
+                    </div>
+                </div>
             </div>
         )
     }
