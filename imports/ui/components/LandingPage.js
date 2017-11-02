f7533d4df1abdd0fe343a044b886e0b0c80d8e2b
adelavegaf <antoniodlavga@gmail.com>
2017-11-01 21:50:54 -0500

    Fix layout to remove horizontal scroll on Windows.

--- imports/ui/components/LandingPage.js
+++ imports/ui/components/LandingPage.js
@@ -61,52 +61,55 @@ const subHeader = {
 export default class LandingPage extends Component {
     render() {
         return (
-            <div>
-                <div className="row justify-content-center" style={loginPromptContainerStyle}>
+            <div className="row">
+                <div className="col-12" style={loginPromptContainerStyle}>
                     <div className="row justify-content-center" style={headersContainer}>
                         <h1 className="col-12" style={mainHeader}>Manage your time in the web</h1>
-                        <h2 className="col-12" style={subHeader}>Set website usage daily goals and start getting insights today!</h2>
+                        <h2 className="col-12" style={subHeader}>Set website usage daily goals and start getting
+                            insights today!</h2>
                     </div>
-                    <div className="col-10 col-md-6 col-lg-4">
-                        <div/>
-                        <div style={loginContainer}>
-                            <Card>
-                                <CardText>
-                                    <div style={loginPromptStyle}>
-                                        Welcome
-                                    </div>
-                                    <div className="row justify-content-center" style={textFieldContainer}>
-                                        <TextField type="text"
-                                                   floatingLabelText="Username"
-                                                   floatingLabelFixed={true}
-                                                   errorText={this.props.authenticationError}
-                                                   value={this.props.username}
-                                                   onChange={(e) => this.props.onUserNameChange(e.target.value)}/>
-                                    </div>
-                                    <div className="row justify-content-center" style={textFieldContainer}>
-                                        <TextField type="password"
-                                                   floatingLabelText="Password"
-                                                   floatingLabelFixed={true}
-                                                   errorText={this.props.authenticationError}
-                                                   value={this.props.password}
-                                                   onChange={(e) => this.props.onPasswordChange(e.target.value)}/>
-                                    </div>
-                                    <div className="row justify-content-center" style={buttonsContainer}>
-                                        <div style={buttonContainer}>
-                                            <RaisedButton label="Log In"
-                                                          secondary={true}
-                                                          labelStyle={labelButtonStyle}
-                                                          onClick={() => this.props.onLogIn()}/>
+                    <div className="row justify-content-center">
+                        <div className="col-10 col-md-6 col-lg-4">
+                            <div/>
+                            <div style={loginContainer}>
+                                <Card>
+                                    <CardText>
+                                        <div style={loginPromptStyle}>
+                                            Welcome
                                         </div>
-                                        <div style={buttonContainer}>
-                                            <RaisedButton label="Register"
-                                                          secondary={true}
-                                                          labelStyle={labelButtonStyle}
-                                                          onClick={() => this.props.onRegister()}/>
+                                        <div className="row justify-content-center" style={textFieldContainer}>
+                                            <TextField type="text"
+                                                       floatingLabelText="Username"
+                                                       floatingLabelFixed={true}
+                                                       errorText={this.props.authenticationError}
+                                                       value={this.props.username}
+                                                       onChange={(e) => this.props.onUserNameChange(e.target.value)}/>
                                         </div>
-                                    </div>
-                                </CardText>
-                            </Card>
+                                        <div className="row justify-content-center" style={textFieldContainer}>
+                                            <TextField type="password"
+                                                       floatingLabelText="Password"
+                                                       floatingLabelFixed={true}
+                                                       errorText={this.props.authenticationError}
+                                                       value={this.props.password}
+                                                       onChange={(e) => this.props.onPasswordChange(e.target.value)}/>
+                                        </div>
+                                        <div className="row justify-content-center" style={buttonsContainer}>
+                                            <div style={buttonContainer}>
+                                                <RaisedButton label="Log In"
+                                                              secondary={true}
+                                                              labelStyle={labelButtonStyle}
+                                                              onClick={() => this.props.onLogIn()}/>
+                                            </div>
+                                            <div style={buttonContainer}>
+                                                <RaisedButton label="Register"
+                                                              secondary={true}
+                                                              labelStyle={labelButtonStyle}
+                                                              onClick={() => this.props.onRegister()}/>
+                                            </div>
+                                        </div>
+                                    </CardText>
+                                </Card>
+                            </div>
                         </div>
                     </div>
                 </div>
